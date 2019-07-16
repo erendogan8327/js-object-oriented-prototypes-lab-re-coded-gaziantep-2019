2c775629f698adf79f28f7b6b42699a71fc0ba70
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-07-16 19:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState;
   this.training = training;
 
-this.prototype.veto = function(){
+this.veto = function(){
 return 'No, I must disagree';
 };
 this.approve = function(){
